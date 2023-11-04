9e700fcd36802defbf36dc818a8f0d2adc7ddd56
Kyle Lovely <41491421+pianometal@users.noreply.github.com>
2023-11-04 10:06:44 -0400

    Initial Commit

---
+++ ToggleView/ToggleViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleViewApp.swift
+//  ToggleView
+//
+//  Created by Kyle Lovely on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ToggleViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
